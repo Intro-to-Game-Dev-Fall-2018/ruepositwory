fb36f803b736d814bc3fa620996e5a13e5007d30
Rue Laurito <cal707@nyu.edu>
2018-10-21 22:12:45 -0400

    Initial commit (messed up actual initial commit, apparently). (10/21/18).

---
+++ BLOCKADE/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	void Start()
+	{
+		coroutine = Move(Vector2.Up);
+		StartCoroutine(coroutine);
+	}
+
+	private IEnumerator Move(Vector2 direction)
+	{
+		transform.position += direction;
+		//Wait time is the duration between snakes' moves
+		yield return new WaitForSeconds(0.5f);
+	}
+}
